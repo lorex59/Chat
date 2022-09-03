2053c1eb429d814939a16c4d6879ea08db4faa72
lorex59 <lorex59@mail.ru>
2022-09-03 16:39:28 +1000

    add changeUserName

--- app/src/main/java/com/example/fake/ui/fragments/ChangeUsernameFragment.kt
+++ app/src/main/java/com/example/fake/ui/fragments/ChangeUsernameFragment.kt
@@ -1,59 +1,94 @@
 package com.example.fake.ui.fragments
 
 import android.os.Bundle
+import android.view.*
 import androidx.fragment.app.Fragment
-import android.view.LayoutInflater
-import android.view.View
-import android.view.ViewGroup
+import com.example.fake.MainActivity
 import com.example.fake.R
+import com.example.fake.databinding.FragmentChangeUsernameBinding
+import com.example.fake.ui.utilits.*
+import java.util.*
 
-// TODO: Rename parameter arguments, choose names that match
-// the fragment initialization parameters, e.g. ARG_ITEM_NUMBER
-private const val ARG_PARAM1 = "param1"
-private const val ARG_PARAM2 = "param2"
 
-/**
- * A simple [Fragment] subclass.
- * Use the [ChangeUsernameFragment.newInstance] factory method to
- * create an instance of this fragment.
- */
 class ChangeUsernameFragment : Fragment() {
-    // TODO: Rename and change types of parameters
-    private var param1: String? = null
-    private var param2: String? = null
 
-    override fun onCreate(savedInstanceState: Bundle?) {
-        super.onCreate(savedInstanceState)
-        arguments?.let {
-            param1 = it.getString(ARG_PARAM1)
-            param2 = it.getString(ARG_PARAM2)
-        }
-    }
+    private lateinit var binding: FragmentChangeUsernameBinding
+    lateinit var newUsername: String
 
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View? {
-        // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_change_username, container, false)
+
+        binding = FragmentChangeUsernameBinding.inflate(inflater, container, false)
+        return binding.root
     }
 
-    companion object {
-        /**
-         * Use this factory method to create a new instance of
-         * this fragment using the provided parameters.
-         *
-         * @param param1 Parameter 1.
-         * @param param2 Parameter 2.
-         * @return A new instance of fragment ChangeUsernameFragment.
-         */
-        // TODO: Rename and change types and number of parameters
-        @JvmStatic
-        fun newInstance(param1: String, param2: String) =
-            ChangeUsernameFragment().apply {
-                arguments = Bundle().apply {
-                    putString(ARG_PARAM1, param1)
-                    putString(ARG_PARAM2, param2)
+    override fun onResume() {
+        super.onResume()
+        setHasOptionsMenu(true)
+        binding.settingsInputUsername.setText(USER.username)
+    }
+
+    override fun onCreateOptionsMenu(menu: Menu, inflater: MenuInflater) {
+        (activity as MainActivity).menuInflater.inflate(R.menu.settings_menu_confirm, menu)
+    }
+
+    override fun onOptionsItemSelected(item: MenuItem): Boolean {
+        when (item.itemId) {
+            R.id.settings_confirm_change -> change()
+        }
+        return true
+    }
+
+    private fun change() {
+        newUsername = binding.settingsInputUsername.text.toString().lowercase(Locale.getDefault())
+        if (newUsername.isEmpty()) {
+            showToast("Поле пустое")
+        } else {
+            REF_DATA_BASE_ROOT.child(NODE_USERNAMES)
+                .addListenerForSingleValueEvent(AppValueEventListener {
+                    if (it.hasChild(newUsername)) {
+                        showToast("Такое имя уже занято")
+                    } else {
+                        changeUsername()
+                    }
+                })
+        }
+    }
+
+    private fun changeUsername() {
+        REF_DATA_BASE_ROOT.child(NODE_USERNAMES).child(newUsername).setValue(UID)
+            .addOnCompleteListener {
+                if (it.isSuccessful) {
+                    updateCurrentUsername()
+                }
+            }
+    }
+
+    private fun updateCurrentUsername() {
+        REF_DATA_BASE_ROOT.child(NODE_USERS).child(UID).child(CHILD_USERNAME)
+            .setValue(newUsername)
+            .addOnCompleteListener {
+                if (it.isSuccessful) {
+                    showToast("Данные обновлены")
+                    deleteOldUsername()
+                } else {
+                    showToast(it.exception?.message.toString())
+                }
+            }
+    }
+
+    private fun deleteOldUsername() {
+        REF_DATA_BASE_ROOT.child(NODE_USERNAMES).child(USER.username)
+            .removeValue()
+            .addOnCompleteListener {
+                if (it.isSuccessful) {
+                    showToast("Данные обновлены")
+                    parentFragmentManager.popBackStack()
+                    USER.username = newUsername
+                } else {
+                    showToast(it.exception?.message.toString())
                 }
             }
     }
